dbb5288ff07b464ad6ffa9a1b4336fc2a014d608
Saksham-garg <sakshamgarg142@hotmail.com>
2024-08-11 23:03:08 +0530

    created add friend api

--- src/app/api/friend/add/route.tsx
+++ src/app/api/friend/add/route.tsx
@@ -1,13 +1,15 @@
+import { fetchRedis } from "@/helpers/redis"
 import { authOptions } from "@/utils/auth"
+import db from "@/utils/db"
 import { addFriendValidator } from "@/utils/validators/addFriendValidator"
 import { getServerSession } from "next-auth"
+import { ZodError } from "zod"
 
 
-export default async function POST(req:Request){
+export async function POST(req:Request){
    
     try {
-        const email = await req.json()
-    
+        const { email }= await req.json()
         const validatedEmail = addFriendValidator.parse(email)
     
         if(!validatedEmail){
@@ -23,10 +25,13 @@ export default async function POST(req:Request){
                 cache:'no-store'
             }
         )
+        console.log(`user:email:${validatedEmail.email}`)
+        const idToAdd = ( await fetchRedis('get',`user:email:${validatedEmail.email}`)) as ( string | null)
+    
+        console.log(idToAdd)
+        // const data = ( await requestFriend.json()) as { result: string | null }
 
-        const data = ( await requestFriend.json()) as { result: string | null }
-
-        const isToAdd = data.result
+        // const idToAdd = data.result
 
         const session = await getServerSession(authOptions)
 
@@ -34,16 +39,31 @@ export default async function POST(req:Request){
             return new Response("Unauthorized",{status:401})
         }
 
-        if(!isToAdd){
+        if(!idToAdd){
             return new Response("This person doesn't exist",{status:400})
         }
 
-        if(isToAdd == session.user.id){
+        if(idToAdd == session.user.id){
             return new Response("You cannot add yourself!",{status:400})
         }
+
+        // check if the user already added
+        const isAlreadyAdded = ( await fetchRedis('sismember',`user:${session.user.id}:friends`,idToAdd)) as ( 0 | 1)
+
+        if(isAlreadyAdded){
+            return new Response("This person is already your friend",{ status: 400})
+        }
         
+        // Valid request, send friend request
+
+        db.sadd(`user:${idToAdd}:incoming_friend_request`,session.user.id)
+
+        return new Response('OK')
     } catch (error) {
-        
+        if(error instanceof ZodError){
+            return new Response("Invalid Input payload",{ status: 422})
+        }
+
+        return new Response("Invalid request",{ status:400 })
     }
-    
 }
