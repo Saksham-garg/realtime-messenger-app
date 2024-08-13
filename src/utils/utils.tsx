55a07045968b4361e8c73ffcf5c70f3200215793
Saksham-garg <sakshamgarg142@hotmail.com>
2024-08-13 18:27:45 +0530

    Added chat href link slug

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -5,3 +5,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...input: ClassValue[]){
     return twMerge(clsx(input))
 }
+
+export function chatHrefConstructor(chatId1:string,chatId2:string){
+    const sortedIds = [ chatId1,chatId2 ].sort()
+    return `${sortedIds[0]}--${sortedIds[1]}`
+}
