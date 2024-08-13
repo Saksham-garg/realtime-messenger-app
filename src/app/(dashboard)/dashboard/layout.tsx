aa1f8746dfce4d26799bd10bdf0a7122b12343d3
Saksham-garg <sakshamgarg142@hotmail.com>
2024-08-13 18:25:29 +0530

    Added chatlist on sidebar

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -8,6 +8,8 @@ import { notFound } from 'next/navigation'
 import React from 'react'
 import  FriendRequestsSidebarOptions from '@/components/FriendRequestsSidebarOptions'
 import { fetchRedis } from '@/helpers/redis'
+import getFriendsByUserID from '@/helpers/get-friends-userid'
+import SidebarChatList from '@/components/SidebarChatList'
 
 type Props = {
     children:React.ReactNode
@@ -34,10 +36,10 @@ async function layout({ children }: Props) {
     const session = await getServerSession(authOptions)
 
     if(!session) notFound()
-
+    const friends = await getFriendsByUserID(session.user.id)
     const unseenFriendRequests = (await fetchRedis('smembers',`user:${session.user.id}:incoming_friend_request`) as User[]).length
 
-    console.log("first",unseenFriendRequests)
+
   return (
     <div className='w-full h-screen flex'>
         <div className='flex w-full h-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
@@ -45,14 +47,18 @@ async function layout({ children }: Props) {
                 <Icons.Logo className='h-8 auto text-indigo-600'/>
             </Link>
 
-            <div className="text-xs font-semibold leading-6 text-gray-400">
-                Your chats
-            </div>
-
+           {
+            friends?.length > 0 ? (
+                <div className="text-xs font-semibold leading-6 text-gray-400">
+                    Your chats
+                </div>
+            ):(null)
+           }
             <nav className='flex flex-1 flex-col'>
                 <ul role='list' className='flex flex-1 flex-col gap-y-7'>
-                <li>
-
+                    <li>
+                        <SidebarChatList friends={friends} sessionId={session.user.id}/>
+                    </li>
                     <div className='text-xs font-semibold leading-6 text-gray-400'>
                         Overview
                     </div>
@@ -74,36 +80,35 @@ async function layout({ children }: Props) {
                                 )
                             })
                         }
+                        
+                        <li>
+                            <FriendRequestsSidebarOptions initialFriendRequests={unseenFriendRequests} sessionId={session.user.id} />
+                        </li>
                     </ul>
-                </li>
-                
-                <li>
-                    <FriendRequestsSidebarOptions initialFriendRequests={unseenFriendRequests} sessionId={session.user.id} />
-                </li>
 
-            <li className='-mx-6 mt-auto flex items-center'>
-              <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
-                <div className='relative h-8 w-8 bg-gray-50'>
-                  <Image
-                    fill
-                    referrerPolicy='no-referrer'
-                    className='rounded-full'
-                    src={session.user.image || ''}
-                    alt='Your profile picture'
-                  />
-                </div>
+                    <li className='-mx-6 mt-auto flex items-center'>
+                        <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
+                            <div className='relative h-8 w-8 bg-gray-50'>
+                            <Image
+                                fill
+                                referrerPolicy='no-referrer'
+                                className='rounded-full'
+                                src={session.user.image || ''}
+                                alt='Your profile picture'
+                            />
+                            </div>
 
-                <span className='sr-only'>Your profile</span>
-                <div className='flex flex-col'>
-                  <span aria-hidden='true'>{session.user.name}</span>
-                  <span className='text-xs text-zinc-400' aria-hidden='true'>
-                    {session.user.email}
-                  </span>
-                </div>
-              </div>
+                            <span className='sr-only'>Your profile</span>
+                            <div className='flex flex-col'>
+                            <span aria-hidden='true'>{session.user.name}</span>
+                            <span className='text-xs text-zinc-400' aria-hidden='true'>
+                                {session.user.email}
+                            </span>
+                            </div>
+                        </div>
 
-              <SignOutButton className='h-full aspect-square' />
-            </li>
+                        <SignOutButton className='h-full aspect-square' />
+                    </li>
                 </ul>
             </nav>
         </div>
