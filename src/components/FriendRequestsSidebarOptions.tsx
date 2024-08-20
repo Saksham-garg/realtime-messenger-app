d4f84ccae50bdb9dd78f535a931b55583f3607fd
Saksham-garg <sakshamgarg142@hotmail.com>
2024-08-20 14:17:08 +0530

    added sessionId in useEffect

--- src/components/FriendRequestsSidebarOptions.tsx
+++ src/components/FriendRequestsSidebarOptions.tsx
@@ -26,7 +26,7 @@ const FriendRequestsSidebarOptions = ({ sessionId, initialFriendRequests }: Prop
           pusherClient.unsubscribe(pusherKey(`user:${sessionId}:incoming_friend_requests`))
           pusherClient.unbind('incoming_friend_requests',friendRequestHandler)
         }
-      }, [])
+      }, [sessionId])
     return (
         <Link href={"/dashboard/requests"}
             className='text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'>
